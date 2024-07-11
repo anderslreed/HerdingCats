fbf5ef508e9026d62f2a394c9ee4beb46281e6a4
Anders Lehn Reed <anders.lehn.reed>
2024-07-11 21:28:24 +0200

    Ef relationships

--- HerdingCats/Data/KittyDbContext.cs
+++ HerdingCats/Data/KittyDbContext.cs
@@ -4,11 +4,19 @@ using Microsoft.EntityFrameworkCore;
 namespace HerdingCats.Data
 {
 
-public class KittyDbContext : DbContext
+public class KittyDbContext(DbContextOptions<KittyDbContext> options) : DbContext(options)
 {
     public DbSet<Address> Adresses { get; set; }
 
     public DbSet<Report> Reports { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Address>().HasMany(a => a.Cats).WithOne().HasForeignKey("address");
+        modelBuilder.Entity<Address>().HasMany(a => a.Humans).WithOne().HasForeignKey("address");
+        modelBuilder.Entity<Cat>().HasMany(c => c.Reports).WithOne().HasForeignKey("cat");
+        base.OnModelCreating(modelBuilder);
+    }
 }
 
 }
